2e4c1e6673fd46ec2fc4e9d4e9a430bd81294a46
attarreno <67298388+attarreno@users.noreply.github.com>
2021-01-12 11:23:50 +0700

    ScInstallerV1.sh
    Termux Script Installer

---
+++ ScInstallerV1.sh
@@ -0,0 +1,170 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 2
+echo
+toilet -f big -F gay Attar
+echo $green"Welcome Dude"
+echo $yellow"By Attar Nation"
+sleep 2
+echo
+echo $red"[+]Choose Available Tools :"
+echo
+echo $white"0.)Exit"
+echo
+echo $cyan"1.) Stabile Connection"
+echo
+echo $red"2.)Install Dark fb"
+echo
+echo $white"3.)Install Katoolin"
+echo
+echo $purple"4.)Hack Instagram"
+echo
+echo $blue"5.)Install Red Hawk"
+echo
+echo $green"6.)Install Onex Script"
+echo
+echo $yellow"7.)Install SpamWa"
+echo
+echo $green"8.)Install MrCakil"
+echo
+echo $purple"9.)Install Hack Ig V2"
+read -p "Choose Tools :" Select
+if [ $Select = 1 ] || [ $Select = 1 ]
+then
+clear
+toilet -f big -F gay Attar
+echo $green"Stable Connection"
+sleep 3
+ping -s1000 1.1.1.1
+sh ScInstallerV1.sh
+fi
+
+if [ $Select = 0 ] || [ $Select = 0 ]
+then
+clear
+echo $yellow
+figlet "EXIT"
+sleep 3
+exit
+fi
+
+if [ $Select = 2 ] || [ $Select = 2 ]
+then
+clear
+    echo $green"Installing DarkFb"
+    sleep 2
+    cd $HOME
+    apt update && apt upgrade
+    apt install git
+    apt install python && python2
+    git clone https://github.com/kumpulanremaja/darkfb
+    bash ScInstallerV1.sh
+fi
+
+
+if [ $Select = 3 ] || [ $Select = 3 ]
+then
+clear
+    echo $green"Installing..."
+    sleep 3
+    apt update && apt upgrade
+    apt install git
+    apt install python && apt install python2
+    git clone https://github.com/LionSec/katoolin
+    bash ScInstallerV1.sh
+fi
+
+if [ $Select = 4 ] || [ $Select = 4 ]
+then
+clear
+    echo $cyan"Installing Hack Instagram"
+    apt update && apt upgrade
+    apt install python && apt install python2
+    apt install git
+    git clone git clone https://github.com/saydog/HEK-IG
+    sleep 2
+    echo $purple"Done Installed"
+    sleep 2
+    echo $blue"Script by : Attar Nation"
+    sleep 2
+    echo $red"Subscribe"
+   bash ScInstallerV1.sh
+fi
+
+if [ $Select = 5 ] || [ $Select = 5]
+then
+clear
+    echo $green"Installing Script!"
+    sleep 3
+    apt update && apt upgrade 
+    apt install git
+    git clone https://github.com/Tuhinshubhra/RED_HAWK
+    sleep 2
+    echo $blue"Done Installed"
+    echo $green"Succesfully Installed"
+    sleep 3
+    echo $red"Subscribe Attar Nation"
+    sleep 4
+    bash.ScInstallerV1.sh
+fi
+
+if [ $Select = 6 ] || [ $Select = 6 ]
+then
+clear
+    echo $cyan"Installing..."
+    sleep 2
+    echo $white"Pastikan Sudah Meng-Install Python & Git"
+    sleep 3
+    echo $red"Be sure you Installed Python & Git"
+    sleep 4
+    git clone https://github.com/Rajkumrdusad/onex
+    sleep 5
+    echo $green"Succesfully Installed/Sudah Ter-Install "
+    bash.ScInstallerV1.sh
+fi
+
+if [ $Select =7 ] || [ $Select =7 ]
+then
+clear
+    echo $cyan"Installing"
+    sleep 3
+    echo $puprle"Make sure You install Python & Git"
+    sleep 2
+    pkg update && pkg upgrade
+    pkg install git
+    git clone https://github.com/kumpulanremaja/spam-wa.git
+    sleep 3
+    echo $green"Done!"
+    bash.ScInstallerV1.sh
+fi
+
+if [ $Select ="8" ] || [ $Select ="8" ]
+  echo $cyan"INSTALLING..."
+then
+clear
+     echo $green "PREPARING FOR INSTALL"
+     sleep 2
+     apt upgrade && apt update
+     apt install git
+     git clone https://github.com/Mrcakil/mrcakil
+     sh.ScInstallerV1.sh
+fi
+
+if [ $Select ="9" ] || [ $Select ="9" ]
+then
+   echo $green"INSTALLING"
+   sleep 2
+   echo $puprle"PREPARING PACKAGES..."
+   sleep 3
+   apt update && apt upgrade
+   apt install git
+   git clone https://github.com/indogram/indotagram.git
+   sh.ScInstallerV1.sh
+fi
+
